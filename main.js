747cf428a3b07a63c105cc326a06984b1fc0c0be
ThatFnafguy <85533879+ThatFnafguy@users.noreply.github.com>
2022-02-15 08:36:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+Webcam.set({
+  width:350,
+  height:300,
+  image_format :'png',
+  png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+    
+function take_snapshot()
+{
+  Webcam.snap(function(data_uri) {
+      document.getElementById("result").innerHTML = '<img id="captured_image" src="'+ data_uri+'"/>';
+  });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5DcbWMI3q/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('model Loaded!');
+}
+
+function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+  function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      
+    
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+
+    gesture = results[0].label;
+    
+    toSpeak = "";
+    
+    if(gesture == "class2")
+    {
+      toSpeak = "That was amazing";
+      document.getElementById("result_object_gesture_icon").innerHTML ="&#128522;";
+    }
+    else if(gesture == "class3")
+    {
+      toSpeak = "That is really sad";
+      document.getElementById("result_object_gesture_icon").innerHTML ="&#128532;";
+    }
+    else if(gesture == "class4")
+    {
+      toSpeak = "ohno";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#128548;";
+    }
+
+    speak();
+  }
+}
+
+function speak(){
+  var synth = window.speechSynthesis;
+
+  speak_data = toSpeak;
+
+  var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+  synth.speak(utterThis);
+
+}
